9695b0c644d0aa724e42a013b8bc61518d1b6f2a
Prasant <prasantraj0808@gmail.com>
2022-06-02 10:02:48 +0530

    templates in django

--- home/views.py
+++ home/views.py
@@ -1,11 +1,20 @@
+from multiprocessing import context
 from django.shortcuts import render,HttpResponse
 def home(request):
-    return HttpResponse("this is my home page(/)")
+   # return HttpResponse("this is my home page(/)")
+   context={'name':'Prasant','course':'Django'}
+   return render(request,'home.html',context)
+
 def about(request):
-    return HttpResponse("this is my about page(/about)")
+    #return HttpResponse("this is my about page(/about)")
+    
+    return render(request,'about.html')
+
 def projects(request):
-    return HttpResponse("this is my projects page(/projects)")
+    #return HttpResponse("this is my projects page(/projects)")
+    return render(request,'projects.html')
 def contact(request):
-    return HttpResponse("this is my contact page(/contact)")
+   # return HttpResponse("this is my contact page(/contact)")
+   return render(request,'contact.html')
 
 # Create your views here.
