5063771b62921e8bc029d528b5001170d65d4425
Prasant <prasantraj0808@gmail.com>
2022-06-05 16:34:22 +0530

    changin admin interface in url and models file

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import path,include
 from home import views
 
+admin.site.site_header="PRASANT RAJ"
+admin.site.site_title="Welcome to prasant's dashboard"
+admin.site.index_title="welcome to this portal"
+
 urlpatterns = [
    
     path('',views.home,name='home'),
