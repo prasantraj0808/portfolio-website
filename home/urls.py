84184b1fb444a6471c3ce691c3881f24630df8a4
prasantraj0808 <prasantraj0808@gmail.com>
2022-08-20 03:36:25 +0530

    Hosting added procfile,requirement,added some URL,

--- home/urls.py
+++ home/urls.py
@@ -6,11 +6,18 @@ admin.site.site_header="PRASANT RAJ"
 admin.site.site_title="Welcome to prasant's dashboard"
 admin.site.index_title="welcome to this portal"
 
+
+from django.views.static import serve
+from django.conf.urls import url
+
 urlpatterns = [
     path('admin/',admin.site.urls),
     path('',views.home,name='home'),
     path('about',views.about,name='about'),
     path('projects',views.projects,name='projects'),
-    path('contact',views.contact,name='contact')
+    path('contact',views.contact,name='contact'),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+
 
 ]
